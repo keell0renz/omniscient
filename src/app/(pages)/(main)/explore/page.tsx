313052740b1897e5d2243aefea575611cb4f1db9
sssgen <deskarte.des@gmail.com>
2024-01-20 01:05:24 +0200

    explore page fix

--- src/app/(pages)/(main)/explore/page.tsx
+++ src/app/(pages)/(main)/explore/page.tsx
@@ -3,7 +3,6 @@ import ExploreSearch from "@/components/main/explore/ExploreSearch";
 const page = () => {
     return (
         <div className="home overflow-hidden h-fit bg-transparent mx-auto box-border max-w-10xl w-full -mt-10 flex flex-col">
-            <h1 className="text-center text-6xl font-bold">Search any Project you want to</h1>
             <ExploreSearch />
         </div>
     );
--- src/components/main/explore/ExploreSearch.tsx
+++ src/components/main/explore/ExploreSearch.tsx
@@ -2,6 +2,8 @@
 import { Input } from "@/components/ui/input";
 import { SearchIcon } from "lucide-react";
 import { useToast } from "@/components/ui/use-toast";
+import { Button } from "@/components/ui/button";
+import { ArrowTopRightIcon } from "@radix-ui/react-icons";
 
 const ExploreSearch = () => {
     const { toast } = useToast()
@@ -14,10 +16,49 @@ const ExploreSearch = () => {
         })
     }
 
+    const buttonHardcode = [
+        {
+            id: 1,
+            title: "New project"
+        },
+        {
+            id: 2,
+            title: "Frontend"
+        },
+        {
+            id: 3,
+            title: "Backend"
+        },
+        {
+            id: 4,
+            title: "Marketing"
+        },
+    ];
+
     return (
-        <div className="flex mt-16 justify-center items-center" onClick={() => handleClick()}>
-            <Input className="bg-background outline-none focus-visible:outline-none border border-muted-foreground p-5 mx-2 max-w-[900px]" />
-            <SearchIcon className="w-5 h-5 -translate-x-10 cursor-pointer" />
+        <div className="flex flex-col">
+            <div className="flex mt-16 justify-center items-center" onClick={() => handleClick()}>
+                <Input
+                    placeholder="Search any project you want to.."
+                    className="bg-foreground text-background rounded-full outline-none focus-visible:outline-none border border-muted-foreground p-5 mx-2 max-w-[600px]"
+                />
+                <SearchIcon className="w-5 h-5 -translate-x-10 cursor-pointer text-background" />
+            </div>
+            <div className="flex flex-row flex-nowrap justify-center mt-2 gap-2">
+                {buttonHardcode.map((button) => (
+                    <Button
+                        key={button.id}
+                        variant="ghost"
+                        className="w-fit my-2 h-8 rounded-full"
+                        onClick={() => handleClick()}
+                    >
+                        {button.title}
+                        <span className="ml-2">
+                            <ArrowTopRightIcon />
+                        </span>
+                    </Button>
+                ))}
+            </div>
         </div>
     );
 };
