c8465921be6add43304373409a3c520e63fc775c
sssgen <deskarte.des@gmail.com>
2024-01-20 00:09:32 +0200

    clerk middleware

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+    publicRoutes: [
+        "/",
+        "/mission",
+        "/explore",
+        "/sign-in",
+        "/sign-up",
+    ],
+});
+
+export const config = {
+    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
