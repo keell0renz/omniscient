4d6bee93559e4bc9042111f04838f0c2dad7d24b
Bohdan Agarkov <keell0renz@localhost.localdomain>
2024-01-19 02:18:22 +0100

    Finished working on nodes

--- src/components/main/home/CustomNodes.tsx
+++ src/components/main/home/CustomNodes.tsx
@@ -1,31 +1,19 @@
 import { Handle, Position, NodeProps } from "reactflow";
-import { Info } from "lucide-react";
 
 type CustomNodeData = {
   label: string;
   description: string;
 };
 
-function InfoHoverCard(props: NodeProps<CustomNodeData>) {
-  return (
-    <>
-      <p>{`${props.data.label}`}</p>
-      <div className="mt-1.5">
-        <Info size={16} />
-      </div>
-    </>
-  );
-}
-
 export default function CustomNode(props: NodeProps<CustomNodeData>) {
   return (
     <>
       <div className="flex flex-col rounded-2xl bg-background/70 border w-60 h-28">
-        <div className="w-full h-10 border-b bg-background rounded-t-2xl pl-3 pt-2 font-semibold flex flex-row gap-2 justify-start text-foreground/80 hover:text-foreground">
-          <InfoHoverCard {...props} />
+        <div className="w-full h-10 border-b bg-background rounded-t-2xl pl-3 pt-2 font-semibold text-foreground/80 hover:text-foreground">
+          {`${props.data.label}`}
         </div>
-        <div className="pl-3 p-2 text-sm text-foreground/80">
-          {`${props.data.description}`}
+        <div className="pl-3 p-2 text-sm text-foreground/80 hover:text-foreground">
+        {`${props.data.description}`}
         </div>
       </div>
       <div style={{ visibility: "hidden" }}>
