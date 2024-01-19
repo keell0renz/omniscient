3a22acecb95e1ce46f3c0333e567d3370240ad0e
Bohdan Agarkov <keell0renz@localhost.localdomain>
2024-01-19 03:13:26 +0100

    Added toaster for in development message.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/Theme-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
   title: "Cognitar",
@@ -31,6 +32,7 @@ export default function RootLayout({
         >
           <ThemeProvider attribute="class" defaultTheme="dark">
             {children}
+            <Toaster />
           </ThemeProvider>
         </ClerkProvider>
       </body>
