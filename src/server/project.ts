209ab616ea8b2adb75b68cb38b85c0e0755290be
Bohdan Agarkov <keell0renz@localhost.localdomain>
2024-01-21 14:55:42 +0100

    Added backend for projects

--- src/server/project.ts
+++ src/server/project.ts
@@ -4,6 +4,7 @@ import prisma from "@/lib/prisma"
 import { Project } from "@prisma/client"
 import { unstable_cache } from "next/cache"
 import { revalidatePath, revalidateTag } from "next/cache"
+import { redirect } from "next/navigation"
 import { 
     validateCreateProject, 
     validateSetAIContext, 
@@ -11,6 +12,7 @@ import {
     SetAIContextSchema 
 } from "@/schema/project"
 import { auth } from "@clerk/nextjs"
+import { error } from "console"
 
 export async function getPublicProjects(): Promise<Project[]> {
     return await unstable_cache(
@@ -35,7 +37,7 @@ export async function getProjectsByUser(): Promise<Project[]> {
     const { userId } = auth()
 
     if (!userId) {
-        throw Error("Not authenticated!")
+        throw new Error("Not authenticated!")
     }
 
     return await unstable_cache(
@@ -59,7 +61,7 @@ export async function getProjectById(project_id: string): Promise<Project | null
     const { userId } = auth()
 
     if (!userId) {
-        throw Error("Not authenticated!")
+        throw new Error("Not authenticated!")
     }
 
     return await unstable_cache(
@@ -78,4 +80,176 @@ export async function getProjectById(project_id: string): Promise<Project | null
         undefined,
         { tags: [`project_${project_id}`], revalidate: 60 * 60 }
     )()
+}
+
+export async function createProject(schema: CreateProjectSchema) {
+    const { userId } = auth()
+
+    if (!userId) {
+        throw Error("Not authenticated!")
+    }
+
+    const validated = validateCreateProject.safeParse(schema)
+
+    if (!validated.success) {
+        throw new Error(`Failed schema validation: ${validated.error}`)
+    }
+
+    try {
+        await prisma.project.create({
+            data: {
+                title: validated.data.title,
+                description: validated.data.description,
+                user_id: userId
+            }
+        })
+
+    } catch (error) {
+        throw new Error(`Failed to create project: ${error}`)
+    }
+
+    revalidatePath("/projects")
+}
+
+export async function editProject(schema: CreateProjectSchema, project_id: string) {
+    const { userId } = auth()
+
+    if (!userId) {
+        throw new Error("Not authenticated!")
+    }
+
+    const validated = validateCreateProject.safeParse(schema)
+
+    if (!validated.success) {
+        throw new Error(`Failed schema validation: ${validated.error}`)
+    }
+
+    try {
+        await prisma.project.update({
+            where: {
+                id: project_id,
+                user_id: userId
+            },
+            data: {
+                title: validated.data.title,
+                description: validated.data.description
+            }
+        })
+    } catch (error) {
+        throw new Error(`Failed to update project: ${error}`)
+    }
+
+    revalidatePath("/projects")
+}
+
+export async function setAIContextForProject(schema: SetAIContextSchema, project_id: string) {
+    const { userId } = auth()
+
+    if (!userId) {
+        throw new Error("Not authenticated!")
+    }
+
+    const validated = validateSetAIContext.safeParse(schema)
+
+    if (!validated.success) {
+        throw new Error(`Failed schema validation: ${validated.error}`)
+    }
+
+    try {
+        await prisma.project.update({
+            where: {
+                id: project_id,
+                user_id: userId
+            },
+            data: {
+                ai_context: validated.data.ai_context
+            }
+        })
+
+    } catch (error) {
+        throw new Error(`Failed to set AI context: ${error}`)
+    }
+
+    revalidatePath("/projects")
+}
+
+export async function setProjectPublicity(published: boolean, project_id: string) {
+    const { userId } = auth()
+
+    if (!userId)
+        throw new Error("Not authenticated!")
+
+    try {
+        await prisma.project.update({
+            where: {
+                id: project_id,
+                user_id: userId
+            },
+            data: {
+                public: published
+            }
+        })
+    } catch (error) {
+        throw new Error(`Failed to set project publicity: ${error}`);
+    }
+    
+    revalidatePath("/projects")
+}
+
+export async function deleteProjectById(project_id: string): Promise<void> {
+    const { userId } = auth()
+
+    if (!userId)
+        throw new Error("Not authenticated!")
+
+    try {
+        await prisma.project.delete({
+            where: {
+                id: project_id,
+                user_id: userId
+            }
+        });
+    } catch (error) {
+        throw new Error(`Failed to delete project: ${error}`);
+    }
+
+    revalidatePath("/projects")
+}
+
+export async function importPublicProject(project_id: string) {
+    const { userId } = auth()
+
+    if (!userId)
+        throw new Error("Not authenticated!")
+
+    try {
+        const parent_project = await prisma.project.findFirst({
+            where: {
+                id: project_id,
+                public: true,
+                NOT: {
+                    user_id: userId
+                }
+            }
+        })
+
+        if (!parent_project) {
+            throw new Error(`Project not found or not public: ${project_id}`);
+        }
+
+        await prisma.project.create({
+            data: {
+                title: parent_project.title,
+                description: parent_project.description,
+                ai_context: parent_project.ai_context,
+                user_id: userId,
+                imported_from_id: parent_project.id
+            }
+        })
+
+    } catch (error) {
+        throw new Error(`Failed to import project: ${error}`)
+    }
+
+    revalidatePath("/projects")
 }
