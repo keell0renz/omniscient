1deb5eb1f454d1f68e0db2d2a14dc28cd39ff0c9
Bohdan Agarkov <keell0renz@localhost.localdomain>
2024-01-22 16:11:31 +0100

    Refactoring

--- src/components/project/panel/PanelSceleton.tsx
+++ src/components/project/panel/PanelSceleton.tsx
@@ -1,6 +1,6 @@
 import ProjectSkeleton from "../cards/Skeleton";
 
-export default async function ProjectPanel() {
+export default async function PanelSkeleton() {
     return (
       <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-4 xl:grid-cols-3">
         <ProjectSkeleton />
