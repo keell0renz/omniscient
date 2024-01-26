1cbbf40bff5009d8c2930fda15284bdfc3d943cc
Bohdan Agarkov <keell0renz@localhost.localdomain>
2024-01-26 01:15:55 +0100

    Added Node

--- src/app/(app)/(project)/p/[project_id]/page.tsx
+++ src/app/(app)/(project)/p/[project_id]/page.tsx
@@ -1,8 +1,7 @@
 export default async function Page({
-    params,
-  }: {
-    params: { project_id: string };
-  }) {
-    return <div>{params.project_id}</div>;
-  }
-  
+  params,
+}: {
+  params: { project_id: string };
+}) {
+  return <div>{params.project_id}</div>;
+}
---
+++ src/components/roadmap/Node.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { Handle, Position, NodeProps } from "reactflow";
+
+type CustomNodeData = {
+  label: string;
+};
+
+export default function CustomNode(props: NodeProps<CustomNodeData>) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <div className="rounded-lg text-black bg-white py-2 px-6">
+        {`${props.data.label}`}
+      </div>
+      <div style={{ visibility: isHovered ? "visible" : "hidden" }}>
+        {[Position.Top, Position.Right, Position.Bottom, Position.Left].map(
+          (pos) => (
+            <Handle
+              key={pos}
+              type="target"
+              position={pos}
+              id={pos.toLowerCase()}
+              className="p-1"
+            />
+          ),
+        )}
+        {[Position.Top, Position.Right, Position.Bottom, Position.Left].map(
+          (pos) => (
+            <Handle
+              key={pos}
+              type="source"
+              position={pos}
+              id={pos.toLowerCase()}
+              className="p-1"
+            />
+          ),
+        )}
+      </div>
+    </div>
+  );
+}
