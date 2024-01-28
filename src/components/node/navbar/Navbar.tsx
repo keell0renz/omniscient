fbc83bb61d77c97ff604c825a4de3520e2f19eec
Bohdan Agarkov <keell0renz@localhost.localdomain>
2024-01-29 00:02:29 +0100

    Designing

--- src/components/node/navbar/Navbar.tsx
+++ src/components/node/navbar/Navbar.tsx
@@ -7,13 +7,38 @@ export default async function Navbar({
   params: { pid: string; nid: string };
 }) {
   return (
-    <div className="h-full w-72 flex flex-col justify-start pt-4 ml-2">
+    <div className="h-full w-72 flex flex-col justify-start pt-4 mx-2">
       <div className="flex flex-col justify-start">
         <NewChat {...params} />
         <AIContext {...params} />
       </div>
-      <div className="h-full flex flex-col justify-start overflow-scroll">
-        <></>
+      <div className="flex flex-col flex-grow overflow-hidden">
+        <div className="flex flex-col pt-2 space-y-1 overflow-y-auto">
+          <p className="text-sm text-gray-400 pl-4">Articles</p>
+          <div className="flex flex-col justify-start ml-2 mr-1">
+            {Array.from({ length: 3 }, (_, index) => (
+              <div
+                key={index}
+                className="text-sm py-2 px-2 rounded-lg hover:bg-secondary"
+              >
+                Article
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col pt-2 space-y-1 overflow-y-auto">
+          <p className="text-sm text-gray-400 pl-4">Chats</p>
+          <div className="flex flex-col justify-start ml-2 mr-1">
+            {Array.from({ length: 5 }, (_, index) => (
+              <div
+                key={index}
+                className="text-sm py-2 px-2 rounded-lg hover:bg-secondary"
+              >
+                New Chat
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
