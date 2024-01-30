a9bf3d809314b2212a41d5419fb0d39b35439aa4
Bohdan Agarkov <keell0renz@localhost.localdomain>
2024-01-30 22:39:47 +0100

    Chat backend

---
+++ src/server/chats.ts
@@ -0,0 +1,64 @@
+"use server"
+
+import prisma from "@/lib/prisma"
+import { Chat, Message } from "@prisma/client"
+import { Message as MessageSchema } from "ai"
+import { revalidatePath } from "next/cache"
+import { auth } from "@clerk/nextjs"
+
+export async function createChat(project_id: string, node_id: string): Promise<Chat> {
+    const { userId } = auth()
+
+    if (!userId) {
+        throw new Error("Not authenticated!")
+    }
+
+    revalidatePath(`/p/${project_id}/n/${node_id}`)
+
+    return await prisma.chat.create({
+        data: {
+            node_id: node_id,
+            project_id: project_id
+        }
+    })
+}
+
+export async function deleteChat(project_id: string, node_id: string, chat_id: string) {
+    const { userId } = auth()
+
+    if (!userId) {
+        throw new Error("Not authenticated!")
+    }
+
+    await prisma.message.deleteMany({
+        where: {
+            chat_id: chat_id
+        }
+    })
+
+    await prisma.chat.delete({
+        where: {
+            id: chat_id
+        }
+    })
+
+    revalidatePath(`/p/${project_id}/n/${node_id}`)
+}
+
+export async function insertMessageIntoChat(message: MessageSchema, project_id: string, node_id: string, chat_id: string) {
+    const { userId } = auth()
+
+    if (!userId) {
+        throw new Error("Not authenticated!")
+    }
+
+    await prisma.message.create({
+        data: {
+            chat_id: chat_id,
+            node_id: node_id,
+            project_id: project_id,
+            role: message.role,
+            content: message.content
+        }
+    })
+}
