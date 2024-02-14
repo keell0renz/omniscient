609c4d0fa66374cd859379354631d02cdd9f6fdb
Bohdan Agarkov <keell0renz@localhost.localdomain>
2024-02-14 16:45:55 +0100

    [DEVT-10] Fix, removed toasters from mutation functions

--- src/mutate/projects.ts
+++ src/mutate/projects.ts
@@ -11,33 +11,23 @@ import {
 } from "@/server/projects";
 import { getUserProjectsKey } from "@/utils/projects";
 
-export async function createProject(schema: CreateProject) {
-    // Create project is done on the /projects page, so we have to manually revalidate useUserProjects()
-    const { toast } = useToast();
+// Handle exceptions in components using toasters or other local messages, alerts, etc!
 
+export async function createProject(schema: CreateProject) {
     try {
         await createProjectWithSchema(schema);
     } catch (error) {
-        toast({
-            title: "An error occurred!",
-            description: `${error}`,
-        });
+        throw error;
     } finally {
         mutate(unstable_serialize(getUserProjectsKey));
     }
 }
 
 export async function importProject(project_id: string) {
-    // After you import the project you redirect user to /projects and it automatically revalidates on mount.
-    const { toast } = useToast();
-
     try {
         await importPublicProject(project_id);
     } catch (error) {
-        toast({
-            title: "An error occurred!",
-            description: `${error}`,
-        });
+        throw error;
     }
 }
 
@@ -45,9 +35,6 @@ export async function editProject(
     project_id: string,
     updatedData: EditProject,
 ) {
-    // Here we update data of useProject(project_id) optimistically.
-    const { toast } = useToast();
-
     try {
         await editProjectById(updatedData, project_id);
         mutate(`project_id:${project_id}`, undefined, {
@@ -55,23 +42,14 @@ export async function editProject(
             rollbackOnError: true,
         });
     } catch (error) {
-        toast({
-            title: "An error occurred!",
-            description: `${error}`,
-        });
+        throw error;
     }
 }
 
 export async function deleteProject(project_id: string) {
-    // After deleting, user is sent again to /projects, and it revalidates automatically on mount.
-    const { toast } = useToast();
-
     try {
         await deleteProjectById(project_id);
     } catch (error) {
-        toast({
-            title: "An error occurred!",
-            description: `${error}`,
-        });
+        throw error;
     }
 }
