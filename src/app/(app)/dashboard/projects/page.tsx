4149eb7aa6fbac121a3e5302b94d8000e10e2858
Bohdan Agarkov <keell0renz@localhost.localdomain>
2024-02-17 15:09:53 +0100

    Fixed hydration error

--- src/app/(app)/dashboard/projects/page.tsx
+++ src/app/(app)/dashboard/projects/page.tsx
@@ -1,4 +1,8 @@
-import Projects from "@/components/dashboard/projects/Projects";
+import dynamic from "next/dynamic";
+
+const Projects = dynamic(() => import("@/components/dashboard/projects/Projects"), {
+  ssr: false,
+});
 
 export default function Page({ params }: { params: { q?: string }}) {
   return (
