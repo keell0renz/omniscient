640795171f044b6d8af485ee8985b917201ee3e3
Bohdan Agarkov <keell0renz@localhost.localdomain>
2024-02-18 13:14:32 +0100

    N

--- src/components/app/project/navbar/NavbarProject.tsx
+++ src/components/app/project/navbar/NavbarProject.tsx
@@ -1,7 +1,18 @@
 import { useProject } from "@/hooks/projects";
 import { useSearchParams } from "next/navigation";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function NavbarProject() {
     const params = useSearchParams()
-    const { data, isLoading } = useProject(params.get("q") as string)
+    const { data, error, isLoading } = useProject(params.get("q") as string)
+
+    if (isLoading || error) return (
+        <Skeleton className="w-20 h-4 my-auto" />
+    )
+
+    return (
+        <p className="mt-1">
+            {data?.title}
+        </p>
+    )
 }
