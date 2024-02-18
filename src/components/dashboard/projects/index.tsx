55c92de8105dd5bd2703d0f666974d297408a787
Bohdan Agarkov <keell0renz@localhost.localdomain>
2024-02-18 12:24:11 +0100

    Moved Search

--- src/components/dashboard/projects/index.tsx
+++ src/components/dashboard/projects/index.tsx
@@ -1,3 +1,3 @@
 export { default as Projects } from "./Projects";
 export { default as AddProject } from "./new/AddProject";
-export { default as Search } from "./new/Search";
+export { default as Search } from "./Search";
