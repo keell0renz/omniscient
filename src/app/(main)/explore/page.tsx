33198d394b73883a50415f35abacd1f1c88293d7
sssgen <deskarte.des@gmail.com>
2024-02-20 00:23:12 +0200

    explore page

--- src/app/(main)/explore/page.tsx
+++ src/app/(main)/explore/page.tsx
@@ -1,3 +1,22 @@
+import dynamic from "next/dynamic";
+import { Search } from "@/components/main/explore";
+
+const Projects = dynamic(
+  () => import("@/components/main/explore/Projects"),
+  {
+    ssr: false,
+  },
+);
+
+
 export default function Page() {
-  return <></>;
+  return (
+    <>
+      <section className="flex w-full h-fit justify-center">
+        <Search />
+      </section>
+
+      <Projects className="mt-24" />
+    </>
+  );
 }
--- src/components/main/explore/Search.tsx
+++ src/components/main/explore/Search.tsx
@@ -1,5 +1,40 @@
 "use client";
 
-export default function Search() {
-  return <></>;
+import { Input } from "@/components/ui/input";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { useSearchParams } from "next/navigation";
+import { useDebounceCallback } from "usehooks-ts";
+
+export default function ExploreSearchInput() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const searchQuery = searchParams.get("q");
+
+  const debouncedSetQuery = useDebounceCallback((query) => {
+    const params = new URLSearchParams(searchParams);
+
+    if (query) {
+      params.set("q", query);
+    } else {
+      params.delete("q");
+    }
+
+    replace(`${pathname}?${params.toString()}`);
+  }, 500);
+
+  return (
+    <div className="h-fit w-full max-w-[1000px] relative flex flex-row items-center">
+      <Input
+        className="rounded-full bg-gray-100 text-black h-12 text-md p-6 focus-visible:ring-0"
+        defaultValue={searchQuery || undefined}
+        placeholder="What do you want to find?"
+        onChange={(e) => {
+          debouncedSetQuery(e.target.value);
+        }}
+      />
+    </div>
+  );
 }
