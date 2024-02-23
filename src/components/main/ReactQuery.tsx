50fd89d0f0b097559b8c797676a80d3548e2185f
Bohdan Agarkov <keell0renz@localhost.localdomain>
2024-02-23 21:15:15 +0100

    Refactored project hooks, switched to react query

--- src/components/main/ReactQuery.tsx
+++ src/components/main/ReactQuery.tsx
@@ -1,15 +1,19 @@
-"use client"
+"use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-export default function QueryProvider({ children }: { children: React.ReactNode }) {
-    return (
-        <QueryClientProvider client={queryClient}>
-            <ReactQueryDevtools initialIsOpen={false} />
-            {children}
-        </QueryClientProvider>
-    )
+export default function QueryProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {children}
+    </QueryClientProvider>
+  );
 }
