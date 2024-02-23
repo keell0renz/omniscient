b1ca22b91d28c0190c3d75dab13f72dfa0df8829
Bohdan Agarkov <keell0renz@localhost.localdomain>
2024-02-24 00:24:14 +0100

    Fix

--- src/components/app/dashboard/navbar/NavbarPath.tsx
+++
@@ -1,9 +0,0 @@
-import { NavbarUser, NavbarPathBase } from "../../shared/navbar";
-
-export default function NavbarPath() {
-  return (
-    <NavbarPathBase>
-      <NavbarUser />
-    </NavbarPathBase>
-  );
-}
